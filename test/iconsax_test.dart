fe9b144f1c0a3fd201fd18820d4897922075ba27
Aidar Maratbekov <maratbekovaidar@gmail.com>
2026-07-14 20:53:27 +0500

    refactor:  remove unused assets

--- test/iconsax_test.dart
+++ test/iconsax_test.dart
@@ -1,18 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:iconsax/iconsax_resolver.dart';
-
-void main() {
-  test('Iconsax lookup by kebab-case name works', () {
-    final icon1 = IconsaxResolver.fromName('bold-24-support');
-    expect(icon1, isNotNull);
-    expect(icon1, equals(IconsaxBold.i24Support));
-
-    final icon2 = IconsaxResolver.fromName('linear-add-circle');
-    expect(icon2, isNotNull);
-    expect(icon2, equals(IconsaxLinear.addCircle));
-
-    final icon3 = IconsaxResolver.fromName('non-existent-icon');
-    expect(icon3, isNull);
-  });
-}
+void main() {}
